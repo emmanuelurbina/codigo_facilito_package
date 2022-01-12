c5705b65a0bdb3308cf3da015875649bdff1f871
Emmanuel <emmanuel.lucio.urbina@gmail.com>
2022-01-12 17:59:34 -0600

    FINAL COMMIT

--- codigofacilito/workshops.py
+++ codigofacilito/workshops.py
@@ -1,6 +1,11 @@
 import requests
 
 def unreleased():
+    """Retorna los proximos talleres en CodigoFacilito.
+    >>> type(unreleased()) == type(dict())
+    True
+    """
+
     response = requests.get("https://codigofacilito.com/api/v2/workshops/unreleased")
 
     if response.status_code == 200:
