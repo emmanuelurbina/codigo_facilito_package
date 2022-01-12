1253186f8a6d76e02c436ba5b5d45e450fc8d244
Emmanuel <emmanuel.lucio.urbina@gmail.com>
2022-01-12 17:28:32 -0600

    workshop

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from codigofacilito import workshops
+from codigofacilito import unreleased
+
 if __name__ == "__main__":
     print('primer taller del 2022')
+    workshops= unreleased()
+    print(workshops)
